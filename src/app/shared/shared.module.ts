03219a55a538b37d88cb52b933d124b71e8af347
cesarCtm <cesa.salinas@gmail.com>
2020-04-16 21:58:21 -0400

    fin de la seccion 7

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
+
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
 import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 
 @NgModule({
+    imports: [
+        RouterModule,
+        CommonModule
+    ],
     declarations: [
         NoPageFoundComponent,
         HeaderComponent,
